662ab64612bb75547e0f5994c8f7b48fb4b66411
rowenarono95 <rowenarono@gmail.com>
2020-11-28 20:50:21 +0300

    add sighup form

---
+++ myawwards/forms.py
@@ -0,0 +1,16 @@
+ 
+ 
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=60)
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
