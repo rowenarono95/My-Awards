9a467ebe381dccd2ae761db042599da9e60765ec
rowenarono95 <rowenarono@gmail.com>
2020-11-28 22:37:27 +0300

    add register method

--- myawwards/views.py
+++ myawwards/views.py
@@ -5,5 +5,19 @@ from django.shortcuts import render
 @login_required(login_url='/accounts/login/')
 def home(request):
     project= Project.all_projects()
-    json_projects = []
-    for project in project:
+    
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
