8121631929c48662eb51edae0a8f1b6ac150847d
rowenarono95 <rowenarono@gmail.com>
2020-11-30 01:42:41 +0300

    add url for profile and new project

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/register/', views.register, name='register'),
     path('search_results',views.search_results, name = 'search_results'),
+    path('profile/', views.profile,name = 'profile'),
+    path('new_project/', views.new_project,name ='new_project'),
 ]
 
 if settings.DEBUG:
